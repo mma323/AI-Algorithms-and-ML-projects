f02a9a69aa517d76fde9faf1f9efe91e5854a1d3
Mikael Johansen Mahlio <mma323@uit.no>
2023-11-03 14:55:41 +0100

    Added function update_q_matrix to Q-Learning agent

--- Q-Learning-Agent/Q-Learning-Agent.py
+++ Q-Learning-Agent/Q-Learning-Agent.py
@@ -12,31 +12,49 @@ reward_matrix : dict = {
 
 q_matrix : dict = {
     "state 1": {
-        "move to state 2" : 0, 
-        "move to state 4" : 0
+        "state 2" : 0, 
+        "state 4" : 0
     },
     "state 2": {
-        "move to state 1" : 0, 
-        "move to state 3" : 0, 
-        "move to state 5" : 0
+        "state 1" : 0, 
+        "state 3" : 0, 
+        "state 5" : 0
     },
     "state 3": {
-        "move to state 2" : 0, 
-        "move to state 6" : 0
+        "state 2" : 0, 
+        "state 6" : 0
     },
     "state 4": {
-        "move to state 1" : 0,
-        "move to state 5" : 0
+        "state 1" : 0,
+        "state 5" : 0
     },
     "state 5": {
-        "move to state 2" : 0,
-        "move to state 4" : 0,
-        "move to state 6" : 0
+        "state 2" : 0,
+        "state 4" : 0,
+        "state 6" : 0
     },
     "state 6": {
-        "move to state 3" : 0,
-        "move to state"   : 0
+        "state 3" : 0,
+        "state 5" : 0
     }
 }
 
+def update_q_matrix(state, action):
+    q_matrix[state][action] = (
+        q_matrix[state][action] + 
+        learning_rate * (
+            reward_matrix[action] + 
+            discount_factor * max(q_matrix[action].values()) - 
+            q_matrix[state][action]
+        )
+    )
 
+update_q_matrix("state 4", "state 5")
+update_q_matrix("state 5", "state 6")
+update_q_matrix("state 4", "state 5")
+update_q_matrix("state 1", "state 4")
+
+
+print(q_matrix["state 2"])
+print(q_matrix["state 3"])
+print(q_matrix["state 5"])
