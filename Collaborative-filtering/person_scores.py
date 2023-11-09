44a9031f645fb5ecb06816759dea4659d5e6091a
Mikael Johansen Mahlio <mma323@uit.no>
2023-11-09 12:58:46 +0100

    Added a dictionary of person scores to be used for collaborative filtering

---
+++ Collaborative-filtering/person_scores.py
@@ -0,0 +1,58 @@
+person_scores = {
+    "person 1": {
+        "vikings": 5,
+        "breaking bad": 5,
+        "westworld": 3,
+        "game of thrones": 4,
+    },
+    "person 2": {
+        "vikings" : 4,
+        "breaking bad": 3,
+        "the sopranos": 3,
+        "westworld": 4,
+        "game of thrones": 5,
+        "the witcher": 2,
+        "skam": 3,
+        "les bureau": 3,
+
+    },
+    "person 3": {
+        "vikings" : 4,
+        "breaking bad": 3,
+        "the sopranos": 3,
+        "westworld": 2,
+        "game of thrones": 2,
+        "the witcher": 3,
+        "skam": 4,
+        "les bureau": 1,
+    },
+    "person 4": {
+        "breaking bad": 3,
+        "the witcher": 5,
+    },
+    "person 5": {
+        "vikings" : 3,
+        "breaking bad": 4,
+        "westworld": 4,
+        "game of thrones": 5,
+        "the witcher": 5,
+        "skam": 1,
+    },
+    "person 6": {
+        "vikings" : 4,
+        "breaking bad": 5,
+        "the sopranos": 2,
+        "westworld": 5,
+        "game of thrones": 3,
+        "the witcher": 2,
+        "skam": 2,
+        "les bureau": 3,
+
+    },
+    "person 7": {
+        "vikings" : 1,
+        "breaking bad": 1,
+        "game of thrones": 2,
+        "the witcher": 1,
+    },
+}
